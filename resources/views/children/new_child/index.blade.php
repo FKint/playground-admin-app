602862eb7f4ae3517a0cffba04a34693cc78e40b
Floris Kint <floris.kint@gmail.com>
2017-05-18 19:00:06 +0100

    Auto focus first name field when creating a new child

--- resources/views/children/new_child/index.blade.php
+++ resources/views/children/new_child/index.blade.php
@@ -2,9 +2,17 @@
 
 @section('content')
 
-<h1>Nieuw kind toevoegen</h1>
-{{ Form::open(['class' => 'form-horizontal', 'id' => 'new-child-form']) }}
-@include('children.forms.child')
-{{ Form::close() }}
+    <h1>Nieuw kind toevoegen</h1>
+    {{ Form::open(['class' => 'form-horizontal', 'id' => 'new-child-form']) }}
+    @include('children.forms.child')
+    {{ Form::close() }}
 
 @endsection
+
+@push('scripts')
+<script>
+    $(function () {
+        $('#new-child-form').find('input[name=first_name]').first().focus();
+    });
+</script>
+@endpush
