beb1733edab0e22299a9d767a8ac5e76083142d1
Floris Kint <floris.kint@gmail.com>
2018-04-28 15:25:47 +0200

    Use unique helper instead of raw query to support other DB systems

--- app/database/migrations/2018_04_28_141846_restrict_unique_week_day_names_to_per_year.php
+++ app/database/migrations/2018_04_28_141846_restrict_unique_week_day_names_to_per_year.php
@@ -2,7 +2,6 @@
 
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
-use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Schema;
 
 class RestrictUniqueWeekDayNamesToPerYear extends Migration
@@ -14,8 +13,8 @@ class RestrictUniqueWeekDayNamesToPerYear extends Migration
      */
     public function up()
     {
-        DB::statement("ALTER TABLE `week_days` ADD UNIQUE( `days_offset`, `year_id`);");
         Schema::table('week_days', function (Blueprint $table) {
+            $table->unique(['days_offset', 'year_id']);
             $table->dropUnique(['name']);
         });
     }
@@ -27,8 +26,8 @@ class RestrictUniqueWeekDayNamesToPerYear extends Migration
      */
     public function down()
     {
-        DB::statement("ALTER TABLE `week_days` DROP INDEX `days_offset`;");
         Schema::table('week_days', function (Blueprint $table) {
+            $table->dropUnique(['days_offset', 'year_id']);
             $table->unique(['name']);
         });
     }
