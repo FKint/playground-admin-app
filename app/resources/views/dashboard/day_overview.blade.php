ba7ddecfdbec4f0176bcd5fd743ce0d4c2c136dc
Floris Kint <floris.kint@gmail.com>
2018-05-05 18:36:25 +0200

    Fix dashboard view to use correct helpers to retrieve all supplements and age groups

--- app/resources/views/dashboard/day_overview.blade.php
+++ app/resources/views/dashboard/day_overview.blade.php
@@ -2,18 +2,18 @@
     <table class="table table-bordered">
         <tr>
             <th rowspan="2"></th>
-            <th colspan="{{ count($all_age_groups) }}" class="text-center">Werking</th>
+            <th colspan="{{ count($year->age_groups) }}" class="text-center">Werking</th>
             <th rowspan="2">Totaal</th>
         </tr>
         <tr>
-            @foreach($all_age_groups as $age_group)
+            @foreach($year->age_groups as $age_group)
                 <th>{{ $age_group->abbreviation }}</th>
             @endforeach
         </tr>
-        @foreach($supplements as $supplement)
+        @foreach($year->supplements as $supplement)
             <tr>
                 <th>{{ $supplement->name }}</th>
-                @foreach($all_age_groups as $age_group)
+                @foreach($year->age_groups as $age_group)
                     <td>
                         <a href="{{ route('internal.registrations_for_date', ['date' => $today_playground_day->date()->format('Y-m-d')]) }}?filter_supplement_id={{$supplement->id}}&filter_age_group_id={{$age_group->id}}">
                             {{ $today_playground_day->count_supplements_for_age_group($supplement, $age_group) }}
@@ -28,7 +28,7 @@
             </tr>
         @endforeach
         <th>Aanwezige kinderen</th>
-        @foreach($all_age_groups as $age_group)
+        @foreach($year->age_groups as $age_group)
             <td>
                 <a href="{{ route('internal.registrations_for_date', ['date' => $today_playground_day->date()->format('Y-m-d')]) }}?filter_age_group_id={{$age_group->id}}">
                     {{ $today_playground_day->count_registrations_for_age_group($age_group) }}
