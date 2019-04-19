26c7bba280c7d9fdc4580b6004b2252f6c2b1300
Floris Kint <floris.kint@gmail.com>
2019-04-19 15:51:57 +0200

    Remove testing_scripts.sh

--- testing_scripts.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-echo "START chromedriver!"
-./vendor/laravel/dusk/bin/chromedriver-linux &
-echo "SERVE artisan!"
-php artisan serve --env=.env &
-echo "START tests"
-php artisan dusk
-echo "DONE"
