91001da69863fec4dfd83b9ede2cb1df6d120933
Floris Kint <floris.kint@gmail.com>
2019-04-27 18:58:03 +0200

    Move User Journeys tests to one file to share common database setup

---
+++ app/tests/Browser/UserJourneysTest.php
@@ -0,0 +1,140 @@
+<?php
+
+namespace Tests\Browser;
+
+use Laravel\Dusk\Browser;
+use Tests\Browser\Pages\InternalDashboardPage;
+use Tests\DuskTestCase;
+
+class UserJourneysTest extends DuskTestCase
+{
+    private $user;
+    private $year;
+    private $normalTariff, $socialTariff;
+    private $ageGroup612, $ageGroupKls;
+    private $existingFamily, $existingChild;
+
+    public function setUp()
+    {
+        parent::setUp();
+        app(\DatabaseSeeder::class)->call(\InitialDataSeeder::class);
+        $this->year = \App\Year::firstOrFail();
+        $this->user = factory(\App\User::class)->create(['organization_id' => $this->year->organization_id]);
+
+        $this->normalTariff = \App\Tariff::whereAbbreviation('NRML')->firstOrFail();
+        $this->socialTariff = \App\Tariff::whereAbbreviation('SCL')->firstOrFail();
+        $this->ageGroup612 = \App\AgeGroup::whereAbbreviation('6-12')->firstOrFail();
+        $this->ageGroupKls = \App\AgeGroup::whereAbbreviation('KLS')->firstOrFail();
+        $this->existingFamily = factory(\App\Family::class)->create(['year_id' => $this->year->id, 'guardian_first_name' => 'Veronique', 'guardian_last_name' => 'Baeten']);
+        $this->existingChild = factory(\App\Child::class)->create(['year_id' => $this->year->id, 'first_name' => 'Reinoud', 'last_name' => 'Declercq']);
+        factory(\App\ChildFamily::class)->create(['year_id' => $this->year->id, 'family_id' => $this->existingFamily->id, 'child_id' => $this->existingChild->id]);
+    }
+    /**
+     * Test for creating a new family and managing associations with new and existing children.
+     *
+     * @return void
+     */
+    public function testCreateFamily()
+    {
+        $this->browse(function (Browser $browser) {
+            $browser->loginAs($this->user)
+                ->visit(new InternalDashboardPage($this->year->id))
+                ->navigateToFamiliesPage()
+                ->navigateToAddFamilyPage()
+                ->enterAddFamilyFormData('Joran', 'De Wachter', $this->normalTariff->id, "Only speak English", "Dad: +4987676545652")
+                ->submitAddFamilySuccessfully(\App\Family::count()+1)
+                ->assertSeeGuardianName('Joran De Wachter')
+                ->enterAddChildToFamilyFormData("Josje", "De Wachter", 2008, null, "Allergic to peanuts")
+            // TODO(fkint): assert that age group 6-12 is selected
+                ->submitAddChildToFamilySuccessfully()
+                ->assertSeeCurrentChildName('Josje De Wachter')
+                ->enterAddChildToFamilyFormData("Joris", "Janssens", 2013, null, null)
+            // TODO(fkint): assert that age group KLS is selected
+                ->submitAddChildToFamilySuccessfully()
+                ->assertSeeCurrentChildName("Josje De Wachter")
+                ->assertSeeCurrentChildName("Joris Janssens")
+                ->navigateToFamiliesPage()
+                ->navigateToAddFamilyPage()
+                ->enterAddFamilyFormData('Annelies', 'Vandenbroucke', $this->socialTariff->id, "", "")
+                ->submitAddFamilySuccessfully(\App\Family::count()+1)
+                ->assertSeeGuardianName("Annelies Vandenbroucke")
+                ->enterAddChildToFamilyFormData("Rik", "Vandenbroucke", 2012, $this->ageGroup612->id, "")
+                ->submitAddChildToFamilySuccessfully()
+                ->assertSeeCurrentChildName("Rik Vandenbroucke")
+                ->enterAddExistingChildFormData("De Wachter")
+                ->selectAddExistingChildSuggestion("Joris Janssens")
+                ->assertSeeCurrentChildName("Joris Janssens")
+                ->assertSeeCurrentChildName("Rik Vandenbroucke")
+                ->assertDontSeeCurrentChildName("Josje De Wachter");
+            $child1 = \App\Child::where(['first_name' => "Josje", 'last_name' => "De Wachter"])->firstOrFail();
+            $this->assertEquals($this->ageGroup612->id, $child1->age_group_id);
+            $child2 = \App\Child::where(['first_name' => "Joris", 'last_name' => "Janssens"])->firstOrFail();
+            $this->assertEquals($this->ageGroupKls->id, $child2->age_group_id);
+            $child3 = \App\Child::where(['first_name' => "Rik", "last_name" => "Vandenbroucke"])->firstOrFail();
+            $this->assertEquals($this->ageGroup612->id, $child3->age_group_id);
+        });
+    }
+
+    /**
+     * Test for creating a new child and managing associations with new and existing families.
+     *
+     * @return void
+     */
+    public function testCreateChild()
+    {
+        $this->browse(function (Browser $browser) {
+            $browser->loginAs($this->user)
+                ->visit(new InternalDashboardPage($this->year->id))
+                ->navigateToChildrenPage()
+                ->navigateToAddNewChildDialog()
+                ->enterAddChildFormData('Sonja', 'Boonen', 2004, null, null)
+                ->submitAddChildFormSuccessfully()
+                ->assertSeeChildEntryInTable('Sonja', 'Boonen')
+                ->assertSeeEditChildDialogTabFamilies()
+                ->enterAddNewFamilyFormData('Erik', 'Bulcke', $this->socialTariff->id, 'Requires invoice', 'Call: +329878767875')
+                ->submitAddNewFamilySuccessfully('Erik Bulcke')
+                ->assertSeeCurrentFamily('Erik Bulcke')
+                ->enterAddExistingFamilyFormData("Reinoud")
+                ->selectAddExistingFamilySuggestion("Veronique Baeten")
+                ->assertSeeCurrentFamily("Erik Bulcke")
+                ->assertSeeCurrentFamily("Veronique Baeten");
+            $newChild = \App\Child::where(['first_name' => 'Sonja', 'last_name' => 'Boonen'])->first();
+            $this->assertNotNull($newChild);
+            $this->assertEquals(2, $newChild->families()->count());
+            $this->assertNotNull($newChild->families()->where(['guardian_first_name' => 'Veronique', 'guardian_last_name' => 'Baeten'])->first());
+            $family2 = $newChild->families()->where(['guardian_first_name' => 'Erik', 'guardian_last_name' => 'Bulcke'])->first();
+            $this->assertNotNull($family2);
+
+            $browser->deleteCurrentFamily($family2->id)
+                ->assertDontSeeCurrentFamily("Erik Bulcke");
+            $this->assertNull($newChild->families()->where(['guardian_first_name' => 'Erik', 'guardian_last_name' => 'Bulcke'])->first());
+
+            $browser->enterAddExistingFamilyFormData("Erik Bulcke")
+                ->selectAddExistingFamilySuggestion("Erik Bulcke")
+                ->assertSeeCurrentFamily("Erik Bulcke")
+                ->assertSeeCurrentFamily("Veronique Baeten");
+            $this->assertNotNull($newChild->families()->where(['guardian_first_name' => 'Erik', 'guardian_last_name' => 'Bulcke'])->first());
+
+            $browser->navigateToEditCurrentFamilyDialog($family2->id)
+                ->enterEditFamilyForm("Jonas", null, null, null, null)
+                ->submitEditFamilyFormSuccessfully();
+            $family2->refresh();
+            $this->assertEquals('Jonas', $family2->guardian_first_name);
+            $this->assertEquals('Bulcke', $family2->guardian_last_name);
+        });
+    }
+
+    /**
+     * Test for the settings page.
+     *
+     * @return void
+     */
+    public function testShowsSections()
+    {
+        $this->browse(function (Browser $browser) {
+            $browser->loginAs($this->user)
+                ->visit(new InternalDashboardPage($this->year->id))
+                ->navigateToSettingsPage();
+        });
+    }
+}
