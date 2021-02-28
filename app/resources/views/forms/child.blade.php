d99a85f4b60b6e26d8db47bc93a9466fa586be1f
Floris Kint <floris.kint@gmail.com>
2021-02-28 22:46:06 +0100

    Rename 'leeftijdsgroep' to 'werking' for children

--- app/resources/views/forms/child.blade.php
+++ app/resources/views/forms/child.blade.php
@@ -5,7 +5,7 @@
     {{ Form::bsText('first_name', 'Voornaam', (isset($readonly) && $readonly)?['readonly']:[]) }}
     {{ Form::bsText('last_name', 'Naam', (isset($readonly) && $readonly)?['readonly']:[]) }}
     {{ Form::bsNumber('birth_year', 'Geboortejaar', (isset($readonly) && $readonly)?['readonly']:[]) }}
-    {{ Form::bsDropdown('age_group_id', 'Leeftijdsgroep', ['0' => 'Werking'] + $year->getAllAgeGroupsById(), (isset($readonly) && $readonly)?['readonly']:[]) }}
+    {{ Form::bsDropdown('age_group_id', 'Werking', ['0' => 'Werking'] + $year->getAllAgeGroupsById(), (isset($readonly) && $readonly)?['readonly']:[]) }}
     {{ Form::bsTextarea('remarks', 'Opmerkingen', (isset($readonly) && $readonly)?['readonly']:[]) }}
     @if(!isset($readonly) || !$readonly)
         {{ Form::bsSubmit() }}
