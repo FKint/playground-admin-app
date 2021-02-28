9f9ab129303755fcc24f2ab5048f43edfd6a67a2
Floris Kint <FKint@users.noreply.github.com>
2021-02-28 22:55:42 +0100

    Fix #52: Dutch labels for forms (#66)

--- app/resources/views/forms/family.blade.php
+++ app/resources/views/forms/family.blade.php
@@ -4,13 +4,13 @@
     @if(isset($with_id) && $with_id)
         {{ Form::bsText('id', null, ['readonly']) }}
     @endif
-    {{ Form::bsText('guardian_first_name', null, (isset($readonly) && $readonly)?['readonly']:[]) }}
-    {{ Form::bsText('guardian_last_name', null, (isset($readonly) && $readonly)?['readonly']:[]) }}
-    {{ Form::bsDropdown('tariff_id', null, $year->getAllTariffsById(), (isset($readonly) && $readonly)?['readonly']:[]) }}
-    {{ Form::bsTextarea('remarks', null, (isset($readonly) && $readonly)?['readonly']:[]) }}
-    {{ Form::bsTextarea('contact', null, (isset($readonly) && $readonly)?['readonly']:[]) }}
-    {{ Form::bsTextarea('social_contact', null, (isset($readonly) && $readonly)?['readonly']:[]) }}
-    {{ Form::bsCheckbox('needs_invoice', null, (isset($readonly) && $readonly)?['readonly']:[]) }}
+    {{ Form::bsText('guardian_first_name', 'Voornaam', (isset($readonly) && $readonly)?['readonly']:[]) }}
+    {{ Form::bsText('guardian_last_name', 'Naam', (isset($readonly) && $readonly)?['readonly']:[]) }}
+    {{ Form::bsDropdown('tariff_id', 'Tarief', $year->getAllTariffsById(), (isset($readonly) && $readonly)?['readonly']:[]) }}
+    {{ Form::bsTextarea('remarks', 'Opmerkingen', (isset($readonly) && $readonly)?['readonly']:[]) }}
+    {{ Form::bsTextarea('contact', 'Contactgegevens', (isset($readonly) && $readonly)?['readonly']:[]) }}
+    {{ Form::bsTextarea('social_contact', 'Contact sociaal tarief', (isset($readonly) && $readonly)?['readonly']:[]) }}
+    {{ Form::bsCheckbox('needs_invoice', 'Factuur', (isset($readonly) && $readonly)?['readonly']:[]) }}
     @if(!isset($readonly) || !$readonly)
         {{ Form::bsSubmit(isset($submit_text) ? $submit_text : "Opslaan") }}
     @endif
