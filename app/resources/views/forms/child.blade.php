5ebc63b71639c785b0b5c8f15c36a224d8cc90d2
Floris Kint <floris.kint@gmail.com>
2021-02-28 22:44:57 +0100

    Use Dutch labels for forms. Fix #59

--- app/resources/views/forms/child.blade.php
+++ app/resources/views/forms/child.blade.php
@@ -2,11 +2,11 @@
 @extends('forms.form')
 
 @section('form-content')
-    {{ Form::bsText('first_name', null, (isset($readonly) && $readonly)?['readonly']:[]) }}
-    {{ Form::bsText('last_name', null, (isset($readonly) && $readonly)?['readonly']:[]) }}
-    {{ Form::bsNumber('birth_year', null, (isset($readonly) && $readonly)?['readonly']:[]) }}
-    {{ Form::bsDropdown('age_group_id', null, ['0' => 'Werking'] + $year->getAllAgeGroupsById(), (isset($readonly) && $readonly)?['readonly']:[]) }}
-    {{ Form::bsTextarea('remarks', null, (isset($readonly) && $readonly)?['readonly']:[]) }}
+    {{ Form::bsText('first_name', 'Voornaam', (isset($readonly) && $readonly)?['readonly']:[]) }}
+    {{ Form::bsText('last_name', 'Naam', (isset($readonly) && $readonly)?['readonly']:[]) }}
+    {{ Form::bsNumber('birth_year', 'Geboortejaar', (isset($readonly) && $readonly)?['readonly']:[]) }}
+    {{ Form::bsDropdown('age_group_id', 'Leeftijdsgroep', ['0' => 'Werking'] + $year->getAllAgeGroupsById(), (isset($readonly) && $readonly)?['readonly']:[]) }}
+    {{ Form::bsTextarea('remarks', 'Opmerkingen', (isset($readonly) && $readonly)?['readonly']:[]) }}
     @if(!isset($readonly) || !$readonly)
         {{ Form::bsSubmit() }}
     @endif
